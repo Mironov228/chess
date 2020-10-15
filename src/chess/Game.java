3e8190d8f1db3a9a7c71dbd7fd7d16593546f7c2
Gleb <gleb76549@gmail.com>
2020-10-15 20:40:21 +0300

    add stalemate, made it so that kings couldn't kill each other

--- src/chess/Game.java
+++ src/chess/Game.java
@@ -3,6 +3,7 @@ package chess;
 import chess.Piece.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Game {
@@ -12,6 +13,7 @@ public class Game {
     public KingStatus ks;
     public List<Square> possibleMoves;
     public GameStatus status;
+    public boolean[] castles;
     public Coord getCoordWhiteKing() {
         return board.whiteKing.coord;
     }
@@ -31,6 +33,7 @@ public class Game {
          selectedFigure=null;
          possibleMoves=new ArrayList<>();
          whoWalks = PieceColor.WHITE;
+         castles = new boolean[]{true, true, true, true};
          board.start();
     }
 
@@ -55,6 +58,7 @@ public class Game {
 
     public void pressLeft(Coord coord) {
         Square square = get(coord);
+        System.out.println(Arrays.toString(castles));
         if (selectedFigure == null)
             ifSelectedFigureEqualNull(square);
         else
@@ -128,6 +132,15 @@ public class Game {
         boolean found = false;
 
 
+        List<Coord> coords = Ranges.getCoordsAroundSquare(king.coord);
+
+        for (Coord arnd : coords) {
+            Square tmp = get(arnd);
+            if (tmp!=null&&tmp.get()!=null&&tmp.get().type==PieceType.WHITE_KING)
+            {
+                return true;
+            }
+        }
 
         int[][] pawns = {
                 {1, 1},
@@ -138,14 +151,10 @@ public class Game {
             Square tmp = get(new Coord(x + o[0], y + o[1]));
             if (tmp != null && tmp.get() != null) {
                 if (tmp.get().type == PieceType.WHITE_PAWN) {
-                    found = true;
-                    break;
+                    return true;
                 }
             }
         }
-
-        if (found)
-            return found;
 
         int[][] knights = {
                 {-2, 1},
@@ -163,13 +172,9 @@ public class Game {
             Square tmp = get(new Coord(x+o[0], y+o[1]));
             if (tmp!=null&&tmp.get()!=null)
                 if (tmp.get().type == PieceType.WHITE_KNIGHT) {
-                    found = true;
-                    break;
+                    return true;
                 }
         }
-
-        if (found)
-            return found;
 
         int[][] directions = {
                 {1, 1, 0},
@@ -181,7 +186,7 @@ public class Game {
                 {0, 1, 0},
                 {0, -1, 0}
         };
-        Label : for (int i = 1; i<8; i++) {
+        for (int i = 1; i<8; i++) {
             for (int[] o: directions) {
                 if (o[2] == 1) {
                     continue;
@@ -205,23 +210,28 @@ public class Game {
                     t=PieceType.WHITE_ROOK;
                 if (tmp.get().type == t || tmp.get().type == PieceType.WHITE_QUEEN)
                 {
-                    found=true;
-                    break Label;
+                    return true;
                 } else
                     o[2] = 1;
             }
         }
 
-
-
-
-        return found;
+        return false;
     }
 
     public boolean checkBanForWhite() {
         Square king = Board.whiteKing;
         int x = king.coord.x, y = king.coord.y;
-        boolean found = false;
+
+        List<Coord> coords = Ranges.getCoordsAroundSquare(king.coord);
+
+        for (Coord arnd : coords) {
+            Square tmp = get(arnd);
+            if (tmp!=null&&tmp.get()!=null&&tmp.get().type==PieceType.BLACK_KING)
+            {
+                return true;
+            }
+        }
 
         int[][] pawns = {
                 {1, -1},
@@ -232,14 +242,10 @@ public class Game {
             Square tmp = get(new Coord(x + o[0], y + o[1]));
             if (tmp != null && tmp.get() != null) {
                 if (tmp.get().type == PieceType.BLACK_PAWN) {
-                    found = true;
-                    break;
+                    return true;
                 }
             }
         }
-
-        if (found)
-            return found;
 
         int[][] knights = {
                 {-2, 1},
@@ -257,13 +263,9 @@ public class Game {
             Square tmp = get(new Coord(x+o[0], y+o[1]));
             if (tmp!=null&&tmp.get()!=null)
                 if (tmp.get().type == PieceType.BLACK_KNIGHT) {
-                    found = true;
-                    break;
+                    return true;
                 }
         }
-
-        if (found)
-            return found;
 
         int[][] directions = {
                 {1, 1, 0},
@@ -299,14 +301,13 @@ public class Game {
                     t=PieceType.BLACK_ROOK;
                 if (tmp.get().type == t || tmp.get().type == PieceType.BLACK_QUEEN)
                 {
-                    found=true;
-                    break Label;
+                    return true;
                 } else
                     o[2] = 1;
             }
         }
 
-        return found;
+        return false;
     }
 
     public void ifSelectedFigureEqualNull(Square square) {
@@ -321,13 +322,43 @@ public class Game {
         }
     }
 
+    private void moveRook(int num) {
+        Square mov;
+        Square tmp;
+        switch (num) {
+            case 0:
+                tmp = get(new Coord(0, 7));
+                mov = get(new Coord(3, 7));
+                move(mov, tmp);
+                break;
+            case 1:
+                tmp = get(new Coord(7, 7));
+                mov = get(new Coord(5, 7));
+                move(mov, tmp);
+                break;
+            case 2:
+                tmp = get(new Coord(0, 0));
+                mov = get(new Coord(3, 0));
+                move(mov, tmp);
+                break;
+            case 3:
+                tmp = get(new Coord(7, 0));
+                mov = get(new Coord(5, 0));
+                move(mov, tmp);
+                break;
+        }
+    }
+
     public void move(Square pm, Square selectedFigure) {
         pm.set(selectedFigure.get());
-        if (selectedFigure.get() instanceof King) {
+        if (selectedFigure.get() instanceof King)
+        {
             if (selectedFigure.get().color == PieceColor.WHITE)
+            {
                 Board.whiteKing = pm;
-            else
+            } else {
                 Board.blackKing = pm;
+            }
         }
         selectedFigure.remove();
     }
@@ -338,12 +369,50 @@ public class Game {
             if (pm.equals(square))
             {
                 isPossible = true;
-                move(pm, selectedFigure);
+                if (selectedFigure.get() instanceof Rook)
+                {
+                    Coord t = selectedFigure.coord;
+                    if (t.equals(new Coord(0, 7)))
+                        castles[0]=false;
+                    else if (t.equals(new Coord(7, 7)))
+                        castles[1]=false;
+                    else if (t.equals(new Coord(0,0)))
+                        castles[2]=false;
+                    else if (t.equals((new Coord(7, 0))))
+                        castles[3]=false;
+                }
+                if (selectedFigure.get() instanceof King) {
+
+                    if (selectedFigure.get().color == PieceColor.WHITE){
+                        if (Board.whiteKing.coord.equals(new Coord(4,7))) {
+                            if (pm.coord.equals(new Coord(2, 7)))
+                                moveRook(0);
+                            if (pm.coord.equals(new Coord(6, 7)))
+                                moveRook(1);
+                        }
+                        Board.whiteKing = pm;
+                        castles[0] = false;
+                        castles[1] = false;
+                    }
+                    else {
+                        if (Board.blackKing.coord.equals(new Coord(4,0)))
+                        {
+                            if (pm.coord.equals(new Coord(2,0)))
+                                moveRook(2);
+                            if (pm.coord.equals(new Coord(6,0)))
+                                moveRook(3);
+                        }
+                        Board.blackKing = pm;
+                        castles[2] = false;
+                        castles[3] = false;
+                    }
+                }
                 if ((pm.coord.y==0 || pm.coord.y==7) && pm.get() instanceof Pawn)
                 {
                     Piece fig = new Queen(pm.get().color);
                     pm.set(fig);
                 }
+                move(pm, selectedFigure);
                 this.selectedFigure = null;
                 possibleMoves = new ArrayList<>();
                 whoWalks = whoWalks == PieceColor.WHITE? PieceColor.BLACK : PieceColor.WHITE;
